83345204ed85bf541f6a23d9211767b7adedf7c3
Dylan D <dore.dylan31@gmail.com>
2023-03-09 19:18:42 +0100

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+User.destroy_all
+Store.destroy_all
+StoreShelf.destroy_all
+Product.destroy_all
+
+
+  5.times do
+    User.create!(
+      email: Faker::Internet.email,
+    )
+  end
+
+  User.create!(
+    email: 'driveambrosia@gmail.com',
+    admin: true,
+  )
+
+
+  Store.create!(
+    name: Faker::Company.name,
+    img_url: Faker::LoremFlickr.image(size: "300x300"),
+    admin: User.find_by(email:'driveambrosia@gmail.com'),
+  )
+
+
+
+
+  # Création de 10 rayons pour chaque magasin
+  10.times do
+    StoreShelf.create!(
+      name: Faker::Commerce.department,
+      store: Store.all.sample
+    )
+  end
+
+
+# Création de 40 produits
+  40.times do
+  Product.create!(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.sentence,
+    quantity: Faker::Number.between(from: 1, to: 100),
+    volume: Faker::Number.between(from: 1, to: 10),
+    volume_type: rand(4),
+    price: Faker::Commerce.price,
+    img_url: Faker::LoremFlickr.image(size: "300x300"),
+    store_shelf: StoreShelf.all.sample,
+  )
+  end
