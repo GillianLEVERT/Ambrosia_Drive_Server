29d881e069789aa297d226d030efa3edbea22317
Dylan D <dore.dylan31@gmail.com>
2023-03-20 15:54:29 +0100

    fix smtp and JWT bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp-relay.sendinblue.com',
+    :port => 587,
+    :user_name => ENV['SENDINBLUE_LOGIN'],
+    :password => ENV['SENDINBLUE_PWD'],
+    :authentication => 'login',
+    :enable_starttls_auto => true
+  }
 end
