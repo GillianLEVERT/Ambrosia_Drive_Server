68e68e7c6d582438b9f1fe3e14ee1a1e42cd8b54
MaevaGilles <maevagilles-web@hotmail.fr>
2023-03-27 05:23:41 +0200

    Edit mailer connect

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   def magic_link(user, login_link)
     @user = user
     @login_link  = login_link
-    mail to: @user.email, subject: 'Connexion sur notre site.'
+    mail to: @user.email, subject: 'Voici ton lien pour commencer tes courses.'
   end
 end
